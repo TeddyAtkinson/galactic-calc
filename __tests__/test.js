210f7d8df6dec6160bcd8191051aeb55c6608cd6
teddyatkinson <tedddy.atkinson@gmail.com>
2022-11-14 13:47:07 -0800

    Return age on another planet: good fail

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,8 +1,12 @@
 import { UserInfo } from "../src/calc";
 
 describe("UserInfo", () => {
-  test("store current age of user input", () =>{
+  test("Store current age of user input", () =>{
     let user1 = new UserInfo("20");
     expect(user1.currAge).toEqual("20");
+  });
+  test("Output planetary age of user input", () => {
+    let user1 = new UserInfo("20");
+    expect(user1.murcAge).toEqual()
   });
 });
--- src/calc.js
+++ src/calc.js
@@ -1,5 +1,16 @@
 export class UserInfo {
   constructor(currAge) {
     this.currAge = currAge;
+    this.murcAge = (currAge / .24);
+    this.venusAge = (currAge / .62);
+    this.marsAge = (currAge / 1.88);
+    this.jupAge = (currAge / 11.86);
+  }
+
+  planetCalc(currAge) {
+    // this.murcAge = (currAge / .24);
+    // this.venusAge = (currAge / .62);
+    // this.marsAge = (currAge / 1.88);
+    // this.jupAge = (currAge / 11.86);
   }
 }
