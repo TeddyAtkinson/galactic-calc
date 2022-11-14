92dbb8a766f67b730f7cde8c64dced84ed226103
teddyatkinson <tedddy.atkinson@gmail.com>
2022-11-14 11:59:34 -0800

    UserInfo: good Fail

---
+++ __tests__/test.js
@@ -0,0 +1,8 @@
+import { UserInfo } from "../src/calc";
+
+describe("UserInfo", () => {
+  test("store current age of user input", () =>{
+    let user1 = new UserInfo();
+    expect(user1).toEqual();
+  });
+});
